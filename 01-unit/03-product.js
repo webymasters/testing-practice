176a9fb731ee8caac62dbab91b60a1c6a7866dcb
Manpreet Singh <56766829+webymasters@users.noreply.github.com>
2020-04-06 17:29:17 -0400

    Added testings of Product

--- 01-unit/03-product.js
+++ 01-unit/03-product.js
@@ -1,8 +1,10 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function product() {
-
+function product(number1, number2) {
+  if (number1 === undefined) number1 = 0;
+  if (number2 === undefined) number2 = 1;  
+  return number1 * number2;
 }
 
 // ==================================================
@@ -20,28 +22,37 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should return 0 when either of the numbers are 0.
+  var result = product(0, 2);
+  if (result !== 0) throw new Error('Expected product(0, 2) to be 0. Received: ' + result);
 
+  var result = product(2, 0);
+  if (result !== 0) throw new Error('Expected product(2, 0) to be 0. Received: ' + result);
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // It should ignore additional numbers.
+  var result = product(2, 2, 2, 2, 2);
+  if (result !== 4) throw new Error('Expected product(2, 2,..n) to be 4. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
-
+  var result = product(2);
+    if (result !== 2) throw new Error('Expected product(2) to be 2. Received: ' + result);
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = product();
+  if (result !== 0) throw new Error('Expected product() to be 0. Received: ' + result);
 
-  console.log('All tests passed successfully.');
+  console.log('All tests have passed successfully.');
 
 // ==================================================
 // PRINT ERRORS
 // ==================================================
 } catch (e) {
-  console.warn('Whoops, the following test did not pass:');
+  console.warn('Oops,Something went wrong:');
   console.error(e.message);
 }
