ebf2f0fd1afc66543eacb852e4ff35e10cac0c4b
Manpreet Singh <56766829+webymasters@users.noreply.github.com>
2020-04-06 17:49:34 -0400

    Made calculator with testings

--- 02-integration/01-calc.js
+++ 02-integration/01-calc.js
@@ -1,8 +1,14 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function calc() {
-
+function calc(Op, number1, number2) {
+  switch (Op) {
+    case 'add': return number1 +number2;
+    case 'subtract': return number1 - number2;
+    case 'multiply': return number1 * number2;
+    case 'divide': return number1 / number2;
+    default: return 'Operator not found';
+  }
 }
 
 // ==================================================
@@ -20,28 +26,33 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should return the correct difference when the user provides: 'subtract', 20, 10.
-
+  var result = calc('subtract', 20, 10);
+  if (result !== 10) throw new Error('Expected calc("subtract", 20, 10) to be 10. Received: ' + result);
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // It should return the correct product when the user provides: 'multiply', 9, 9.
-
+  var result = calc('multiply', 9, 9);
+  if (result !== 81) throw new Error('Expected calc("multiply", 9, 9) to be 81. Received: ' + result);
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // It should return the correct quotient when the user provides: 'divide', 9, 3.
-
+  var result = calc('divide', 9, 3);
+  if (result !== 3) throw new Error('Expected calc("divide", 9, 3) to be 3. Received: ' + result);
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // It should return the message 'Operation not supported.' when the user provides: exponent, 2, 8.
-
-  console.log('All tests passed successfully.');
+ // result=calc('exponent', 2, 8);
+ // if (result==='error') throw new Error('Operator not found');
+ 
+  console.log('All tests have passed successfully.');
 
 // ==================================================
 // PRINT ERRORS
 // ==================================================
 } catch (e) {
-  console.warn('Whoops, the following test did not pass:');
+  console.warn('Oops,something went wrong :');
   console.error(e.message);
 }
